44d80161bf4b643df0640aceae2fe848836cd3ff
Zeph Lafassett <zeph@lafassett.com>
2014-05-12 08:38:37 -0700

    Adding default test

---
+++ test/default.js
@@ -0,0 +1,11 @@
+var should = require('should');
+
+describe('Default Client Tests: ', function () {
+
+	describe('Initializing Test Suite:', function () {
+		it('should not fail', function (done) {
+			done();
+		});
+	});
+
+});
