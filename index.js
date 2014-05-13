bf2f533faa264d24403b0500e5ab490d83a55aa1
Zeph Lafassett <zeph@lafassett.com>
2014-05-12 18:02:54 -0700

    Adding methods to get and search for content

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("done");
+var Food2Fork = require('./lib/food-2-fork');
+
+var food = new Food2Fork();
+
+// food.search({
+// 	search: 'chicken'
+// });
+
+food.get({
+	id: '47746'
+});
