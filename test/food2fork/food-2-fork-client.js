00c37f39d675ba862cdc8d76e03a2d60b985638f
Zeph Lafassett <zeph@lafassett.com>
2014-05-13 08:35:10 -0700

    Adding tests for detecting back config data

--- test/food2fork/food-2-fork-client.js
+++ test/food2fork/food-2-fork-client.js
@@ -1,13 +1,38 @@
 var should = require('should'),
-	Food2Fork = require('../../lib/food-2-fork');
+	Food2Fork = require('../../lib/food-2-fork'),
+	Errors = require('../../lib/errors'),
+	F2Fconfig = require('../../config/food2fork.json');
 
 describe('Food2Fork Client Tests: ', function () {
 
 	describe('Initializing Module Tests:', function () {
-		it('should not throw an exception', function (done) {
+		it('should throw an exepection when we do not pass a config file', function (done) {
 			(function () {
 				var f = new Food2Fork();
-			}).should.not.throw();
+			}).should.throw();
+			done();
+		});
+		it('should throw an "Configuration data is missing" exception', function (done) {
+			var emptyConfig = {};
+			(function () {
+				var f = new Food2Fork(emptyConfig);
+			}).should.not.throw('Configuration data is missing');
+			done();
+		});
+		it('should throw an "API Key property is missing in the configuration data" expection', function (done) {
+			var missingKeyConfig = {};
+			(function () {
+				var f = new Food2Fork(missingKeyConfig);
+			}).should.throw('API Key property is missing in the configuration data');
+			done();
+		});
+		it('should throw an "Host property is missing in the configuration data" expection', function (done) {
+			var missingHostConfig = {
+				key: 'empty'
+			};
+			(function () {
+				var f = new Food2Fork(missingHostConfig);
+			}).should.throw('Host property is missing in the configuration data');
 			done();
 		});
 	});
